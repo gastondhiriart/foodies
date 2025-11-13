68849755e0f2a0082a01460f85b62cbe6358d5b5
Gaston d'Hiriart <gastondh91@gmail.com>
2025-11-13 19:08:05 -0300

    Update Next.js version to 16.0.3 and configure react-compiler plugin

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  plugins: ["react-compiler"],
+  rules: {
+    "react-compiler/react-compiler": "error",
+  },
+};
 
 module.exports = nextConfig;
